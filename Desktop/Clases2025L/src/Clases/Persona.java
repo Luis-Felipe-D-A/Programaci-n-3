f06b65f657f91b2e8092503bf39826a1b68fecf7
Estudiantes <Estudiantes@RAIOUJBGBWM3>
2025-08-09 11:23:53 -0500

    Primer commit

---
+++ Desktop/Clases2025L/src/Clases/Persona.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+
+public class Persona {
+    
+   private int cedula;
+   private String nombre;
+   private String apellido;
+   private int edad;
+   private String direccion;
+   private int telefono;
+   
+   
+   public Persona() {
+       
+       System.out.println("Llamado al conductor sin argumento");
+   
+   }
+   public Persona(int cedula,String nombre){
+       
+       System.out.println("Su nombre es: " + nombre +" Su cedula es: " + cedula);
+   }
+
+    /**
+     * @return the cedula
+     */
+    public int getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public int getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+   
+   
+}
---
+++ Desktop/Clases2025L/src/Main.java
@@ -0,0 +1,31 @@
+
+import Clases.Persona;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Llamdo al contructor sin argumentos
+        Persona P = new Persona();
+        
+        P.setCedula(123456777);
+        System.out.println("El valos asignado actualmente a la cedula es:"+P.getCedula());      
+        
+//Llamado al contructor con argumentos
+        Persona p2 = new Persona(104883202,"Obama");
+        
+    }
+    
+}
